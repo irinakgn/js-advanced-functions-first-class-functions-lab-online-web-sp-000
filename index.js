da8c95fd0f26f8e48d95a0988b08f30f16140b38
Irina Kagan <irinagamb@gmail.com>
2019-10-22 01:39:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function returnLastTwoDrivers() {
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(num){
-  if(num ===2) return
-  else () =>{}
+  if(num ===2) return () =>{}
+  else if(num === 5) return num * num
 }
 
 function fareQuintupler() {
